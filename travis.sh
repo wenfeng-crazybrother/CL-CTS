d27f700e4883c323773bd1e54824f8bb8ca4b629
Kevin Petit <kevin.petit@arm.com>
2020-02-28 13:37:07 +0000

    WIP

--- travis.sh
+++ travis.sh
@@ -4,11 +4,21 @@ set -e
 
 export TOP=$(pwd)
 
+TOOLCHAIN_FILE=${TOP}/toolchain.cmake
+
 # Prepare arm toolchain if needed
 if [[ ${JOB_ARCHITECTURE} == arm ]]; then
     wget https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
     tar xvf gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
     export PATH=${TOP}/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin:${PATH}
+
+    echo "SET(CMAKE_SYSTEM_NAME Linux)" >> ${TOOLCHAIN_FILE}
+    echo "SET(CMAKE_SYSTEM_PROCESSOR arm)" >> ${TOOLCHAIN_FILE}
+    echo "SET(CMAKE_C_COMPILER   arm-linux-gnueabihf-gcc)" >> ${TOOLCHAIN_FILE}
+    echo "SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)" >> ${TOOLCHAIN_FILE}
+    echo "SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)" >> ${TOOLCHAIN_FILE}
+    echo "SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)" >> ${TOOLCHAIN_FILE}
+    echo "SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)" >> ${TOOLCHAIN_FILE}
 fi
 
 # Prepare headers
@@ -22,7 +32,7 @@ git clone https://github.com/KhronosGroup/OpenCL-ICD-Loader.git
 cd ${TOP}/OpenCL-ICD-Loader
 mkdir build
 cd build
-cmake -DOPENCL_ICD_LOADER_HEADERS_DIR=${TOP}/OpenCL-Headers/ ..
+cmake -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} -DOPENCL_ICD_LOADER_HEADERS_DIR=${TOP}/OpenCL-Headers/ ..
 make
 
 # Get libclcxx
@@ -33,11 +43,12 @@ git clone https://github.com/KhronosGroup/libclcxx.git
 ls -l
 mkdir build
 cd build
-cmake -DCL_INCLUDE_DIR=${TOP}/OpenCL-Headers
-      -DCL_LIB_DIR=${TOP}/OpenCL-ICD-Loader/build
-      -DCL_LIBCLCXX_DIR=${TOP}/libclcxx
-      -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=./bin
-      -DOPENCL_LIBRARIES="-lOpenCL -lpthread"
+cmake -DCL_INCLUDE_DIR=${TOP}/OpenCL-Headers \
+      -DCL_LIB_DIR=${TOP}/OpenCL-ICD-Loader/build \
+      -DCL_LIBCLCXX_DIR=${TOP}/libclcxx \
+      -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} \
+      -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=./bin \
+      -DOPENCL_LIBRARIES="-lOpenCL -lpthread" \
       ..
 make -j2
 
